73d18937cfb0b5b4bc56e87d929ff6b2542bc29e
alvarogonzalez-git <agonzalez297@gmail.com>
2025-01-15 15:10:45 +0000

    Time: 2030 ms (6.47%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -5,5 +5,5 @@ SELECT
 FROM Prices p
 LEFT JOIN UnitsSold u
     ON p.product_id = u.product_id
-    AND u.purchase_date >= p.start_date AND u.purchase_date <= p.end_date
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
