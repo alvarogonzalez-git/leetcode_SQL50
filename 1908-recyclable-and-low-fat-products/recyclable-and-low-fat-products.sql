7a97216ba240aabb1db194fbb79fa85078c2889e
alvarogonzalez-git <agonzalez297@gmail.com>
2025-07-24 12:50:35 +0100

    Time: 457 ms (97.42%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,5 +1,5 @@
-/* Write your T-SQL query statement below */
+# Write your MySQL query statement below
 select product_id
 from Products
-where low_fats = 'Y' 
-    AND recyclable = 'Y'
+where low_fats = "Y"
+    and recyclable = "Y"
