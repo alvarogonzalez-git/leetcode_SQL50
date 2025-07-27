13e03f876df2a10ca720f2ab1d841e5438da1a12
alvarogonzalez-git <agonzalez297@gmail.com>
2025-07-27 12:43:51 +0100

    Time: 533 ms (64.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select
+    employee_id
+    , department_id
+from Employee
+where (employee_id, primary_flag) in (
+    select employee_id,
+           MAX(primary_flag)
+    from Employee
+    group by employee_id
+);
